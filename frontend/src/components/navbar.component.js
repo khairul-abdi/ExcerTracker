39465ca85fd2335a9563387f0a392fdf44def23d
khairul-abdi <kabdi384@gmail.com>
2020-05-05 01:31:29 +0700

    MERN Stack- ExcerTracker-App

---
+++ frontend/src/components/navbar.component.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button> */}
+        <div className="collpase navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Exercises</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">Create Exercise Log</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/user" className="nav-link">Create User</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
